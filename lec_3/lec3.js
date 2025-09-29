a3e5991ab77232cc21599a2f3b369b0698d9411e
gautham seshapalli <seshapalligautham500@gmail.com>
2025-09-29 13:28:37 +0530

    Add files via upload

---
+++ lec_3/lec3.js
@@ -0,0 +1,46 @@
+
+//sum of first n numbers
+// function sum(n){
+//     let ans=0;
+//     for(let i=1;i<=n;i++){
+//         ans=ans+i
+//     }
+//     return ans;
+// }
+
+
+// const ans=sum(5);
+// console.log(ans);
+
+// const fs= require("fs");
+
+// const contents= fs.readFileSync("a.txt" , "utf-8");
+// console.log(contents);
+
+
+// let user={
+//     age:19,
+//     name:"gautham",
+// }
+
+// console.log(user.age);
+
+function timeout(){
+    console.log("alarm ring");
+}
+
+console.log("yoyo");
+
+setTimeout(timeout,1000);
+
+console.log("welcome");
+
+let c=0;
+
+for(let i=0;i<100000;i++){
+    c=c+i;
+
+}
+
+console.log("expensive operation done");
+
